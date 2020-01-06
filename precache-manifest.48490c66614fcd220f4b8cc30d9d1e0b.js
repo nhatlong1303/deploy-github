df543b6275e2cc450b44d60cd510c6ba555bd292
Nhat Long <nguyennhatlong1303@gmail.com>
2020-01-06 12:59:42 +0700

    Updates

---
+++ precache-manifest.48490c66614fcd220f4b8cc30d9d1e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ca4db10e8083faf8bf748a1742eb88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc6575b1598effea7c9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "22942cc88f94b01c1d49",
+    "url": "/static/js/2.51455a1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.51455a1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fc6575b1598effea7c9",
+    "url": "/static/js/main.64c2cc96.chunk.js"
+  },
+  {
+    "revision": "74f6eb4e8d8401f4d24e",
+    "url": "/static/js/runtime-main.dd20876e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
